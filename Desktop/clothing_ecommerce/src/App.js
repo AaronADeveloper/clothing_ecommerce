b0127670a082c371c78e2aa6d501536f22e29c4a
AaronADeveloper <aaronadeveloper@gmail.com>
2020-10-10 16:16:39 -0400

    created collections previews with styling

--- Desktop/clothing_ecommerce/src/App.js
+++ Desktop/clothing_ecommerce/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Homepage from './pages/Homepage'
 import '../src/App.css'
 import {Switch, Route} from 'react-router-dom';
+import ShopPage from '../src/pages/shop/shopPage'
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div>
     <Switch>
       <Route exact path='/' component={Homepage} />
+      <Route path='/shop' component={ShopPage} />
       
     </Switch>  
     </div>
