44d5efb8b8cbe5e2b095ba8b04bc9eda1f2416b5
AaronADeveloper <aaronadeveloper@gmail.com>
2020-10-11 19:23:07 -0400

    completed sign-in and sign-up forms

--- Desktop/clothing_ecommerce/src/components/sign-in/sign-in.js
+++ Desktop/clothing_ecommerce/src/components/sign-in/sign-in.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import '../sign-in/sign-in.styles.scss'
 import FormInput from '../form-input/form-input'
 import CustomButton from '../custom-button/custom-button'
-import {signInWithGoogle} from '../../firebase/firebase-utils'
+import {signInWithGoogle, auth} from '../../firebase/firebase-utils'
 
 export class SignIn extends Component {
     constructor(props) {
@@ -14,11 +14,20 @@ export class SignIn extends Component {
         }
     }
     
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
 
-        this.setState({email: '', password: ''})
+        const { email, password } = this.state
+
+        try {
+          await auth.signInWithEmailAndPassword(email, password);  
+          this.setState({email: '', password: ''})
+        } catch (error) {
+            console.log(error)
+        }
     }
+
+        
 
     handleChange = event => {
         const {value, name} = event.target
